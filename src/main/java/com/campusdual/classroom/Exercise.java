99241bfe296f24569695826789e998da44f45c46
Juan Ignacio Torres Ichaso <jitorresichaso@macrotest.es>
2025-10-28 19:59:48 +0100

    Exercise 12 solved

--- src/main/java/com/campusdual/classroom/Exercise.java
+++ src/main/java/com/campusdual/classroom/Exercise.java
@@ -2,7 +2,17 @@ package com.campusdual.classroom;
 
 public class Exercise {
     public static void main(String[] args) {
+                Car myCar = new Car();
+                myCar.start();
+                myCar.accelerate();
+                myCar.accelerate();
+                myCar.turnAngleOfWheels(20);
+                myCar.showDetails();
+                myCar.brake();
 
-    }
+                myCar.setReverse(true);
+                myCar.stop();
+                myCar.showDetails();
+            }
+        }
 
-}
